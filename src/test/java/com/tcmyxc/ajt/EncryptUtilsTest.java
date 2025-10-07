fa64bd6420108012b75a4c113b299b1707202ca8
tcmyxc <1282494272@qq.com>
2025-10-07 18:34:02 +0800

    加密工具类

---
+++ src/test/java/com/tcmyxc/ajt/EncryptUtilsTest.java
@@ -0,0 +1,10 @@
+package com.tcmyxc.ajt;
+
+public class EncryptUtilsTest {
+
+    public static void main(String[] args) {
+        System.out.println(EncryptUtils.getMd5("tcmyxc"));
+        System.out.println(EncryptUtils.getDualMd5WithSalt("tcmyxc"));
+        System.out.println(EncryptUtils.getDualMd5WithSalt("tcmyxc", "salt"));
+    }
+}
