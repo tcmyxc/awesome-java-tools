b0b98c317847687212aea92f302bda3017201187
tcmyxc <1282494272@qq.com>
2025-10-08 11:24:38 +0800

    StringUtils判空优化

--- src/main/java/com/tcmyxc/ajt/StringUtils.java
+++ src/main/java/com/tcmyxc/ajt/StringUtils.java
@@ -8,10 +8,10 @@ package com.tcmyxc.ajt;
 public class StringUtils {
 
     /**
-     * 判断字符串是否为空
+     * 判断字符串是否为空，null或者""都返回true
      */
     public static boolean isEmpty(String str) {
-        return str == null || str.trim().isEmpty();
+        return str == null || str.isEmpty();
     }
 
     /**
@@ -19,5 +19,16 @@ public class StringUtils {
      */
     public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否为空或者空白字符串，null，""，" "都返回true
+     */
+    public static boolean isBlank(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
     }
 }
--- src/test/java/com/tcmyxc/ajt/EncryptUtilsTest.java
+++ src/test/java/com/tcmyxc/ajt/EncryptUtilsTest.java
@@ -3,6 +3,7 @@ package com.tcmyxc.ajt;
 public class EncryptUtilsTest {
 
     public static void main(String[] args) {
+        System.out.println(EncryptUtils.getMd5("  "));
         System.out.println(EncryptUtils.getMd5("tcmyxc"));
         System.out.println(EncryptUtils.getDualMd5WithSalt("tcmyxc"));
         System.out.println(EncryptUtils.getDualMd5WithSalt("tcmyxc", "salt"));
